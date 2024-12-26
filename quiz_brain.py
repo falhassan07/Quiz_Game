0afdbb426089b5df723d0288129fc77adf430515
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-12-25 22:55:59 -0500

    added functionality to keep track of score

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,17 +3,23 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0 
 
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number} {current_question.text}. (True/False): ")
-        return user_answer
+        self.check_answer(user_answer, current_question.answer)
 
     
-    def check_answer(self):
-        pass
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong!")
+        print(f"Current Score: {self.score}/{self.question_number}")
 
 
     def still_has_questions(self) -> bool:
